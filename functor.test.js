28b44b55e7e39a9066df2e213714789442feb0d6
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-07-09 02:34:30 +0200

    test with functor matcher

--- functor.test.js
+++ functor.test.js
@@ -3,17 +3,39 @@ const just = require('./functor.js')
 test('equational reasoning (identity)', () => {
 
 	expect(
-		functorToBe( just(1) )
+		getFunctorValue( just(1) )
 	).toBe(
-		functorToBe( just(1) )
+		getFunctorValue( just(1) )
 	)
 
 });
 
+expect.extend({
+  /** Compare the two values inside two functors with Object.is
+   * @method
+   * @augments jest.Matchers
+   * @param {*} actual The functor you want to test.
+   * @param {*} expected The functor you expect.
+   */
+  functorToBe (actual, expected) {
+    const actualValue = getFunctorValue(actual)
+    const expectedValue = getFunctorValue(expected)
+    const pass = Object.is(actualValue, expectedValue)
+    return {
+      pass,
+      message () {
+        return `expected ${actualValue} of ${actual} to ${pass ? '' : 'not'} be ${expectedValue} of ${expected}`
+      }
+    }
+  }
+})
+
+test('equational reasoning (identity)', () => {
+  expect(just(1)).functorToBe(just(1))
+})
 
 
-
-function functorToBe (func) {
+function getFunctorValue (func) {
 	let a
 
 	func.fmap(a => b = a)
